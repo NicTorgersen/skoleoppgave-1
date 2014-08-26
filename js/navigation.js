ff0f36ceefed1e8571d7d8cce6fb7710ae7f92f1
Nichiatu <fristurt@hotmail.com>
2014-08-26 02:06:48 +0200

    first commit

---
+++ js/navigation.js
@@ -0,0 +1,18 @@
+﻿var open    = false,
+    btn     = document.getElementById('navigation-toggle'),
+    bar     = document.getElementById('navigation'),
+    cn      = 'hidden';
+
+function updateElement(isOpen, elm, cn) {
+    if (!isOpen) {
+        elm.className += ' ' + cn;
+    } else {
+        elm.className = elm.className.replace(/(?:^|\s)hidden(?!\S)/g, '');
+    }
+}
+
+btn.onclick = function() {
+    open = !open;
+    updateElement(open, bar, cn);
+    console.log('jeje');
+}
